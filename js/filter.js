ab74be55e41d1338f2c78900246df83d0e8d4264
senghor.khen <senghor.khen@PNC.LAN>
2020-02-09 18:49:15 +0700

    challenge-05

--- js/filter.js
+++ js/filter.js
@@ -1,15 +1,16 @@
-const liElement = document.getElementsByTagName('li');
-
-for(let i = 0; i < liElement.length; i++){
-    // textContent can get all html and all values/ yl tae vlues in page to display
-    // console.log(liElement[i].textContent);
-}
-
 // onFilterInput or onKyePress
 function onFilterInput(){
-    // let resultValue = document.getElementById('inputFieldId').value;
     let inputElement = document.querySelector('input');
     let filter = inputElement.value.toUpperCase();
-    console.log(filter);
+    let textValue;
+    // console.log(filter);
+    const liElement = document.getElementsByTagName('li');
+    for(let i = 0; i < liElement.length; i++){
+        textValue = liElement[i].textContent.toUpperCase() || liElement[i].innerText.toUpperCase();
+        // console.log(liElement[i].textContent);
+        // liElement[i].textContent.toUpperCase().indexOf(filter);
+        // console.log(liElement[i].textContent.toUpperCase());
+        console.log(textValue.indexOf(filter));
+    }
 }
 document.addEventListener('keypress',onFilterInput);
