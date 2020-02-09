12c0d3d2e7609cfba0cbd698ab79018fe8ec16d2
senghor.khen <senghor.khen@PNC.LAN>
2020-02-09 18:39:54 +0700

    challenge-03

--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,7 @@
 const liElement = document.getElementsByTagName('li');
 
 for(let i = 0; i < liElement.length; i++){
-    console.log(liElement[i]);
+    // textContent can get all html and all values/ yl tae vlues in page to display
+    // console.log(liElement[i].textContent);
+    console.log(liElement[i].innerText);
 }
