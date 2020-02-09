c824b48653d71ce508d12eeb0897f1b61fade6ef
senghor.khen <senghor.khen@PNC.LAN>
2020-02-09 18:45:06 +0700

    challenge-04

--- js/filter.js
+++ js/filter.js
@@ -3,5 +3,13 @@ const liElement = document.getElementsByTagName('li');
 for(let i = 0; i < liElement.length; i++){
     // textContent can get all html and all values/ yl tae vlues in page to display
     // console.log(liElement[i].textContent);
-    console.log(liElement[i].innerText);
 }
+
+// onFilterInput or onKyePress
+function onFilterInput(){
+    // let resultValue = document.getElementById('inputFieldId').value;
+    let inputElement = document.querySelector('input');
+    let filter = inputElement.value.toUpperCase();
+    console.log(filter);
+}
+document.addEventListener('keypress',onFilterInput);
